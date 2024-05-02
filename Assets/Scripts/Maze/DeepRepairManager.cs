9422f7a1518a46ce8dfa978c1135145aceb8465c
bottlefish <781230111@qq.com>
2024-05-02 10:22:28 +0800

    把规则和数据抽象出来了

--- Assets/Scripts/Maze/DeepRepairManager.cs
+++ Assets/Scripts/Maze/DeepRepairManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class DeepRepairManager : MonoBehaviour
@@ -7,9 +8,19 @@ public class DeepRepairManager : MonoBehaviour
     // Start is called before the first frame update
     public List<string> logs = new List<string> { "愤怒", "伤心", "害羞" };
     private int currentLogIndex = 0;
+    public int GetCurrentLogIndex()
+{
+    return currentLogIndex;
+    
+}
+public List<string> submoduleNames = new List<string>();
+public int submoduleCount = 0;
+
+
     private MazeGenerator maze;
     private GameObject player;
     public GameObject playerPrefab;
+     private IDeepRepairRule currentRule;
 
 
     
@@ -24,48 +35,38 @@ void OnDisable()
 }
 
 private void Start() {
-    startReapir();
+    startReapir(new Rule1(),logs);
 }
-public void startReapir()
+public void startReapir(IDeepRepairRule rule,List<string> names)
 {
+    SetRule(rule);
+    submoduleNames=names;
+    submoduleCount=names.Count;
     maze=GetComponent<MazeGenerator>();
-    maze.GenerateMaze(10,10);
-
-    
+    maze.GenerateMaze(10,10);  
 }
-
-void HandlePlayerReachTarget(MazeGenerator.Cell targetCell)
+public void IncrementLogIndex()
+{
+    currentLogIndex++;
+}
+public void SetRule(IDeepRepairRule rule)
+{
+    currentRule = rule;
+}void HandlePlayerReachTarget(MazeGenerator.Cell targetCell)
 {
     
-    // Check if the target cell has a submodule
-    if (targetCell.submodule != null)
+   currentRule.HandlePlayerReachTarget(targetCell, this);
+    if (currentRule.CheckIfRepairFinished(this))
     {
-       
-        // Get the TextMeshPro component from the submodule
-        var textMesh = targetCell.submodule.screenText;
-
-        // If textMesh is not null and not already enabled
-        if (!targetCell.submodule.isDefined)
-        {
-            textMesh.text = logs[currentLogIndex];
-            targetCell.submodule.isDefined=true;
-            currentLogIndex++;
-            if (currentLogIndex == logs.Count)
-            {
-                targetCell.submodule.SetRemovable(true);
-                FinishDeepReapir();
-            }
-        }
+        FinishDeepReapir();
     }
 }
-
 
 public void FinishDeepReapir()
     { 
             Debug.Log("Find target");
-            maze.DeleteMaze();
+            //maze.DeleteMaze();
 
-    }
-    
+    }   
 
 }
